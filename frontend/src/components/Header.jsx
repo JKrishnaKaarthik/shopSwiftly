4cd0c3e0c8656cb2f4d033fdb14b94be2ad0b8c0
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-01-20 21:11:34 +0530

    add style to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import cart from "../assets/images/cart-img.png";
+import profile from "../assets/images/profile.png";
 import "./header.css";
 
 export default function Header(props) {
@@ -7,7 +9,9 @@ export default function Header(props) {
       <h1 className="logo">AMAZIN</h1>
       <div className="header-items">
         <input type="text" placeholder="search" className="search" />
-        <img src="../assets/images/cart-img.png" alt="" />
+        <img src={cart} alt="image of cart" className="cart-img" />
+        <button className="your-orders">your orders</button>
+        <img src={profile} alt="profile icon" className="header-profile-icon"/>
       </div>
     </div>
   );
