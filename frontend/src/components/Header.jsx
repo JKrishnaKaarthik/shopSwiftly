1388b1285248c6e565e74de0960f73aae7530d5a
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-01-29 20:09:24 +0530

    add onClick to logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,17 +6,28 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header(props) {
   const navigate = useNavigate();
-  function cartHandleClick(){
+
+  function handleLogoClick() {
+    navigate("/");
+  }
+  function cartHandleClick() {
     //in case if there is a page reload necessary
     // window.location.reload(false);
-    navigate("/cart")
+    navigate("/cart");
   }
   return (
     <div className="header">
-      <h1 className="logo">AMAZIN</h1>
+      <h1 className="logo" onClick={handleLogoClick}>
+        AMAZIN
+      </h1>
       <div className="header-items">
         <input type="text" placeholder="search" className="search" />
-        <img src={cart} alt="image of cart" className="cart-img" onClick={cartHandleClick}/>
+        <img
+          src={cart}
+          alt="image of cart"
+          className="cart-img"
+          onClick={cartHandleClick}
+        />
         <button className="your-orders">your orders</button>
         <button className="header-hello">Hello {props.username}</button>
         <img src={profile} alt="profile icon" className="header-profile-icon" />
