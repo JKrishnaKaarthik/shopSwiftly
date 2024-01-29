6417376a98e5304f90c24245e146eca4b66750bf
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-01-29 20:06:05 +0530

    added better cart images

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { createElement } from "react";
 import Header from "./Header";
 import { cartData } from "../data/cartData";
 import CartItem from "./CartItem";
 
 export default function Cart(props) {
-  console.log(cartData);
+  // console.log(cartData);
+  const cartElements = cartData.map((item) => (
+    <CartItem
+      key={item.id}
+      url={item.url}
+      title={item.title}
+      price={item.price}
+    />
+  ));
   return (
     <div>
-      <Header username={props.username}/>
-      <CartItem />
+      <Header username={props.username} />
+      {cartElements}
     </div>
   );
 }
--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../style/cart.css";
 import img1 from "../cartImages/image1.jpg";
 
-export default function CartItem() {
+export default function CartItem(props) {
   const [quantity, setQuantity] = useState(0);
   function increment() {
     setQuantity((prevQuantity) => prevQuantity + 1);
@@ -12,27 +12,29 @@ export default function CartItem() {
       prevQuantity > 0 ? prevQuantity - 1 : prevQuantity
     );
   }
+
+  // console.log(props);
+  // console.log(`/src/cartImages/${props.url}`)
   return (
     <div className="cart-item">
-      <img src={img1} alt="product image" className="cart-prod-img" />
+      <img src={`/src/cartImages/${props.url}`} alt="product image" className="cart-prod-img" />
       <div className="cart-desc">
         <h4>
-          Amazon Brand - Symactive Full Graphite High Performance Badminton
-          Racquet - Intermediate, S5500 (with Cover) - Black
+          {props.title}
         </h4>
         <p>in Stock</p>
-        <p>1,196.80</p>
+        <p>{props.price}</p>
         <div className="cart-modify">
-            <div className="cart-add-remove">
-              <button className="cart-delete cart-remove" onClick={decrement}>
-                -
-              </button>
-              <h5>{quantity}</h5>
-              <button className="cart-delete cart-add" onClick={increment}>
-                +
-              </button>
-            </div>
-            <button className="cart-delete">delete</button>
+          <div className="cart-add-remove">
+            <button className="cart-delete cart-remove" onClick={decrement}>
+              -
+            </button>
+            <h5>{quantity}</h5>
+            <button className="cart-delete cart-add" onClick={increment}>
+              +
+            </button>
+          </div>
+          <button className="cart-delete">delete</button>
         </div>
       </div>
     </div>
