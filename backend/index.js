f4aed30d246ce9b432a8db5416dbe0df23dde7e9
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-08-04 22:41:15 +0530

    changed the routes from / to /api

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,11 @@ app.use(express.json());
 app.use(express.static("public"));
 const port = process.env.PORT | 5000;
 
-app.use('/', (req, res) => {
-  res.send("server is running")
-});
+// app.use('/', (req, res) => {
+//   res.send("server is running")
+// });
 
-app.post("/", async (req, res) => {
+app.post("/api", async (req, res) => {
   const [result] = await getUser(req.body.userName);
   if (
     result.username === req.body.userName &&
@@ -40,22 +40,22 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.post("/signup", async (req, res) => {
+app.post("/api/signup", async (req, res) => {
   createUser(req.body);
   return res.json("successful data transfer");
 });
 
-app.get("/products", async (req, res) => {
+app.get("/api/products", async (req, res) => {
   const result = await getProducts();
   return res.json(result);
 });
 
-app.get("/product/:id", async (req, res) => {
+app.get("/api/product/:id", async (req, res) => {
   const result = await getProduct(req.params.id);
   return res.json(result);
 });
 
-app.get("/cart/:username", async (req, res) => {
+app.get("/api/cart/:username", async (req, res) => {
   try {
     const result = await getCartItems(req.params.username);
     return res.json({ status: "success", data: result });
@@ -67,7 +67,7 @@ app.get("/cart/:username", async (req, res) => {
   }
 });
 
-app.post("/cart", async (req, res) => {
+app.post("/api/cart", async (req, res) => {
   try {
     const result = await addToCart(req.body.username, req.body.productId);
     return res.json("successful data transfer");
@@ -77,49 +77,49 @@ app.post("/cart", async (req, res) => {
   }
 });
 
-app.delete("/cart/:productID", async (req, res) => {
+app.delete("/api/cart/:productID", async (req, res) => {
   const productId = parseInt(req.params.productID);
   const result = await deleteCartItem(productId);
   return res.json("successful data transfer");
 });
 
-app.put("/cart/increment/:cartId", async (req, res) => {
+app.put("/api/cart/increment/:cartId", async (req, res) => {
   const cartId = parseInt(req.params.cartId);
   const result = await incrementCartCount(cartId);
   return res.json("successful data transfer");
 });
 
-app.put("/cart/decrement/:cartId", async (req, res) => {
+app.put("/api/cart/decrement/:cartId", async (req, res) => {
   const cartId = parseInt(req.params.cartId);
   const result = await decrementCartCount(cartId);
   return res.json("successful data transfer");
 });
 
-app.post("/orders", async (req, res) => {
+app.post("/api/orders", async (req, res) => {
   const result = await addOrder(req.body.username, req.body.productId);
   return res.json(result);
 });
 
-app.get("/orders/:username", async (req, res) => {
+app.get("/api/orders/:username", async (req, res) => {
   const result = await getOrders(req.params.username);
   return res.json(result);
 });
 
-app.get("/search/:query", async (req, res) => {
+app.get("/api/search/:query", async (req, res) => {
   const searchResult = await getSearchResults(req.params.query);
   const query = [searchResult];
   const searchBrands = await getProductBrandFromSearch(query[0]);
   return res.json({ productData: searchResult, brands: searchBrands });
 });
 
-app.get("/search/category/:category", async (req, res) => {
+app.get("/api/search/category/:category", async (req, res) => {
   const searchCategory = await getCategoryProduct(req.params.category);
   const query = [searchCategory];
   const searchBrands = await getProductBrandFromSearch(query[0]);
   return res.json({ productData: searchCategory, brands: searchBrands });
 });
 
-app.post("/search/productFilter", async (req, res) => {
+app.post("/api/search/productFilter", async (req, res) => {
   const productData = req.body.productData.productData;
   const searchRating = req.body.searchRating;
   const searchPrice = req.body.searchPrice;
@@ -134,6 +134,6 @@ app.post("/search/productFilter", async (req, res) => {
   return res.json({ productData: result, brands: brands });
 });
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
